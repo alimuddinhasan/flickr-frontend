55cfe53b3b6fd9fc9beaea3e28b9b24a01a8b427
Alimuddin Hasan <alimudhasan@gmail.com>
2021-03-04 21:18:47 +0700

    Add infinite scroll handling

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 
 import axios from 'plugins/axios'
 
+import Aux from 'components/hoc/hoc'
 import Header from 'components/content/header/header'
 import Feeds from 'components/content/feeds/feeds'
 
@@ -10,16 +11,31 @@ export default function Home() {
   const [search, setSearch] = useState('')
   const [images, setImages] = useState([])
   const [isFirstLoading, setIsFirstLoading] = useState(true)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    if (!images.length || isFirstLoading) {
+    if (loading) {
       fetchImages(search)
     }
-  }, [images])
+  }, [loading])
+
+  useEffect(() => {
+    window.addEventListener('scroll', scrollHandler)
+    return () => window.removeEventListener('scroll', scrollHandler)
+  })
+
+  const scrollHandler = (event) => {
+    // If scroll reach bottom of the page
+    if (!loading && window.pageYOffset >= window.document.body.clientHeight - window.document.body.clientHeight / 4) {
+      console.log('WAKTUNYA LOAD BARU')
+      setLoading(true)
+    }
+  }
 
   const searchHandler = (text) => {
     setSearch(text)
     setImages([])
+    setLoading(true)
   }
 
   const fetchImages = async (search) => {
@@ -34,7 +50,8 @@ export default function Home() {
       })
 
       const { data } = getImages
-      setIsFirstLoading(false)
+      // setIsFirstLoading(false)
+      setLoading(false)
       setImages(prevImages => [...prevImages, ...data.items])
     } catch (err) {
       console.log('SOMETHING WRONG', err)
@@ -42,9 +59,9 @@ export default function Home() {
   }
 
   console.log('IMAGES', images)
+
   return (
-    
-    <div>
+    <Aux>
       <Head>
         <title>Flickrgram | Load Images on Your Scroll</title>
         <link rel="icon" href="/favicon.ico" />
@@ -57,6 +74,6 @@ export default function Home() {
 
       <footer>
       </footer>
-    </div>
+    </Aux>
   )
 }
